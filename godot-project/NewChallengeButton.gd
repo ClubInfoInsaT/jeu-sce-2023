50d2443bb30bd4d43a9acb710c6d052041bac272
scanim-valentin <valentin.guerbette@hotmail.fr>
2022-12-06 15:10:53 +0100

    challenges event

--- godot-project/NewChallengeButton.gd
+++ godot-project/NewChallengeButton.gd
@@ -2,10 +2,15 @@ extends Node2D
 
 export var lifeTime = 5
 var currentTime = 0
-func _ready():
-	pass # Replace with function body.
+func _ready(): 
+	pass
+	
 func _process(delta):
 	if(currentTime > lifeTime) : 
 		queue_free()
 		return
 	currentTime += delta
+
+func _on_TextureButton_pressed():
+	queue_free()
+
--- godot-project/RandomChallenges.gd
+++ godot-project/RandomChallenges.gd
@@ -1,24 +1,28 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+
 const ChallengeButtonScene = "res://NewChallengeButton.tscn"
 var ChallengeButton
 var map
-# Called when the node enters the scene tree for the first time.
+var timeBeforeNewChallenge = 1
+var currentTime = 0
 func _ready():
 	map = find_node("Map")
 	ChallengeButton = preload(ChallengeButtonScene)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+
 func _process(delta):
+	currentTime += delta
+	if currentTime < timeBeforeNewChallenge :
+		return
+	currentTime = 0
 	randomize()
 	var newPos = Vector2(rand_range(0, map.texture.get_width()), rand_range(0, map.texture.get_height()) ) 
 	var challengeButton = ChallengeButton.instance()
 	challengeButton.set_position(newPos)
 	add_child(challengeButton)
 	
-	
+
 	
