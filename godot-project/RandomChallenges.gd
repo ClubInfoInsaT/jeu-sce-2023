aa7f40c5a5c03c42238cd5b1b752cfcd9963508d
scanim-valentin <valentin.guerbette@hotmail.fr>
2022-12-06 13:46:43 +0100

    challenges event

--- godot-project/RandomChallenges.gd
+++ godot-project/RandomChallenges.gd
@@ -4,17 +4,21 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var ChallengeScene 
+const ChallengeButtonScene = "res://NewChallengeButton.tscn"
+var ChallengeButton
 var map
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	map = find_node("Map")
-
+	ChallengeButton = preload(ChallengeButtonScene)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-	#randomize()
-	#var pos_x = rand_range(0, map.get("Sprite").texture.get_width())
-	#var pos_y = rand_range(0, map.get("Sprite").texture.get_heigth())
-	#print( map.get("Sprite"))
+func _process(delta):
+	randomize()
+	var newPos = Vector2(rand_range(0, map.texture.get_width()), rand_range(0, map.texture.get_height()) ) 
+	var challengeButton = ChallengeButton.instance()
+	challengeButton.set_position(newPos)
+	add_child(challengeButton)
+	
+	
 	
