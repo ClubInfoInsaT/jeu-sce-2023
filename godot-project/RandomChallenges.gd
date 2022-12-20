f5fc4266d1cb13e3daabefca9eaa3e6a485f5df7
scanim-valentin <valentin.guerbette@hotmail.fr>
2022-12-20 02:02:09 +0100

    New assets (Pepotrouille)

--- godot-project/RandomChallenges.gd
+++ godot-project/RandomChallenges.gd
@@ -5,7 +5,7 @@ extends Node2D
 const ChallengeButtonScene = "res://NewChallengeButton.tscn"
 var ChallengeButton
 var map
-var timeBeforeNewChallenge = 1
+export(float) var timeBeforeNewChallenge = 1.0
 var currentTime = 0
 func _ready():
 	map = find_node("Map")
@@ -19,7 +19,9 @@ func _process(delta):
 		return
 	currentTime = 0
 	randomize()
-	var newPos = Vector2(rand_range(0, map.texture.get_width()), rand_range(0, map.texture.get_height()) ) 
+	
+	var newPos = Vector2(rand_range(0, map.texture.get_width())*map.scale[0]+map.position[0]-map.texture.get_width()*map.scale[0]/2, rand_range(0, map.texture.get_height())*map.scale[1]+map.position[1]-map.texture.get_height()*map.scale[1]/2 ) 
+	print(newPos)
 	var challengeButton = ChallengeButton.instance()
 	challengeButton.set_position(newPos)
 	add_child(challengeButton)
